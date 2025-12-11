e870974cd1aa3e94c2a23ab7ae7a0ba1804c19bd
simpcl <simpcl2008@gmail.com>
2025-12-11 23:10:27 +0800

    update express-graphql to graphql-http

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
+const { createHandler } = require('graphql-http/lib/use/express');
 const expressPlayground = require('graphql-playground-middleware-express').default;
 const schema = require('./schema/schema');
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', createHandler({
     schema,
     graphiql: true
 }));
